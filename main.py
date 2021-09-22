5edd629160e1e5f10045b23a74ea8b330b077f6f
AContiSG <84248592+AContiSG@users.noreply.github.com>
2021-09-22 19:42:14 -0300

    randomlist1

--- main.py
+++ main.py
@@ -95,6 +95,14 @@ COMANDOS_SIMPLES={
 'pl tato':"https://www.youtube.com/playlist?list=PLFsa3redc-GX4A7yDx9ZjESAz8mt_wWJK",
 'pl busto':"https://www.youtube.com/playlist?list=PLz-W7ibs4AmjFzD3tO4o9DBtVMb-UgZ54",
 'pl yuyu':"https://www.youtube.com/playlist?list=PLXSQn9CA1N0j7Z_8iJLrPUTnl0zG5TpRN",
+'hoal':"sisisaludo",
+'hola':"unsaludo",
+'frasedia':
+["mmmmm yeah","La vida es dura pero mas dura es la vida de los niños sirios, tomá la sopa",
+"momento lol","like si te pasó","sisisi","nah","bueno pero solo a veces","KeyboardInterrupt",
+"jueguen al Rimworld!!","melocoton","¿Cómo motivar a una persona frases? Resultado de imagen para frases motivadoras 101 frases para inspirar y motivar líderes y empleados Algún día es una enfermedad que llevará tus sueños a la tumba contigo ﻿Clic para tuitear.Cuanto más hacemos, más podemo",
+ "Y", "Es muy divertido lograr lo imposible. (????????)"],
+'changelog':"Nuevas funciones: hola, hoal, frasedia y changelog \n Añadida una función que me permite que un numero pueda tener varias rimas (solo está en el 2 para probar) \n Mejoré algunas pavadas \n Le tengo que cambiar el formato a esto!!!",
 'len':longitud
 }
 
@@ -114,7 +122,12 @@ HELP_DICT={
 '$gatotruco':"Cat trick (video)",
 '$pl tato':"Musicarda dou (playlist)",
 '$pl busto':"asdfg op (playlist)",
-'$pl yuyu':"Anime Music (playlist)"
+'$pl yuyu':"Anime Music (playlist)",
+'hola':"unsaludo",
+'hola':"Mas saludos",
+'hoal':"Muchisimos saludos"
+'frasedia':"La frase del momento! (realmente no es una sola por dia lol!)",
+'changelog':"Importantísima funcion que te avisa de todos los muchísimos nuevos cambios de la ultima actualización!!(flashee programador)"
 }
 
 RIMAS1={
