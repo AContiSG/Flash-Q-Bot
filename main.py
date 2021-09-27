5af9df33a19c181a42927fe7c28c3d116c27e13e
Luca Salluzzi <salluzzi.luca@gmail.com>
2021-09-27 13:40:59 -0300

    jf

--- main.py
+++ main.py
@@ -143,7 +143,8 @@ COMANDOS_SIMPLES={
 'frase':FRASEMOT_TUP,
 'len':longitud,
 'invite':"https://discord.com/api/oauth2/authorize?client_id=883480644053774337&permissions=34359856192&scope=bot",
-"git": "https://github.com/AContiSG/Flash-Q-Bot"
+"git": "https://github.com/AContiSG/Flash-Q-Bot",
+'jf': "sos vos cap"
 }
 
 #Comandos que NO devuelven una string (Sin Return)
