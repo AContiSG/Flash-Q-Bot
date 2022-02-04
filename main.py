407d3c0655ee55048cd96b5e5f22181d81aa29ed
AConti <84248592+AContiSG@users.noreply.github.com>
2022-02-04 00:57:29 +0000

    soy lol

--- main.py
+++ main.py
@@ -50,7 +50,7 @@ async def agregar_reaccion(mensaje):
         mensaje_reaccionable = mensaje
 
     emoji = analizar_contenido(mensaje.content, 1)
-    if emoji[0] == ":" and emoji[-1] == ":":
+    if emoji:
         await mensaje_reaccionable.add_reaction(emoji)
 
 async def traducir_mal(mensaje):
